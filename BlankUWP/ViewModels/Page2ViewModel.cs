dce76747a54c90b312ef0529562ff2764c40218c
asNotebook <dev@asWorks.de>
2016-05-16 17:17:18 +0200

    Add project files.

---
+++ BlankUWP/ViewModels/Page2ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlankUWP.ViewModels
+{
+    public class Page2ViewModel //:INotifyPropertyChanged
+    {
+        private ObservableCollection<string> _list;
+
+        public ObservableCollection<string> list
+        {
+            get { return _list; }
+            set { _list = value; }
+        }
+
+
+        
+
+    }
+}

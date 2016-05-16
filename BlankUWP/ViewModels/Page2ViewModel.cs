a35f78ee59407989d64fcc73f3dcf77659f931e9
asNotebook <dev@asWorks.de>
2016-05-16 18:32:07 +0200

    FilledList

--- BlankUWP/ViewModels/Page2ViewModel.cs
+++ BlankUWP/ViewModels/Page2ViewModel.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace BlankUWP.ViewModels
 {
-    public class Page2ViewModel //:INotifyPropertyChanged
+    public class Page2ViewModel :INotifyPropertyChanged
     {
         //Testkommentar
         private ObservableCollection<string> _list;
@@ -19,8 +19,25 @@ namespace BlankUWP.ViewModels
             set { _list = value; }
         }
 
+        public Page2ViewModel()
+        {
+            list = new ObservableCollection<string>();
+            list.Add("Test1");
+            list.Add("Test2");
+            list.Add("Test3");
+            list.Add("Test3");
+        }
 
-        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        } 
 
     }
 }
